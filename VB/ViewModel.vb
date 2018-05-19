bd29ec05b7e64cb21f0e7093594478e730a7318e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/ViewModel.vb
@@ -0,0 +1,45 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+
+Namespace MasterDetailInside
+    Public Class ViewModel
+
+        Private data_Renamed As List(Of ParentTestData)
+        Public ReadOnly Property Data() As List(Of ParentTestData)
+            Get
+                If data_Renamed Is Nothing Then
+                    data_Renamed = New List(Of ParentTestData)()
+                    For i As Integer = 0 To 99
+                        Dim parentTestData As New ParentTestData() With { _
+                            .Text = "Master" & i, _
+                            .Number = i, _
+                            .Data = New List(Of TestData)() _
+                        }
+                        For j As Integer = 0 To 49
+                            parentTestData.Data.Add(New TestData() With { _
+                                .Text = "Detail" & j & " Master" & i, _
+                                .Number = j, _
+                                .Ready = j Mod 2 <> 0 _
+                            })
+                        Next j
+                        data_Renamed.Add(parentTestData)
+                    Next i
+                End If
+                Return data_Renamed
+            End Get
+        End Property
+    End Class
+
+    Public Class TestData
+        Public Property Ready() As Boolean
+        Public Property Text() As String
+        Public Property Number() As Integer
+    End Class
+    Public Class ParentTestData
+        Public Property Text() As String
+        Public Property Number() As Integer
+        Public Property Data() As List(Of TestData)
+    End Class
+End Namespace
---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports DevExpress.Xpf.Grid
+Imports System.Globalization
+Imports System.Windows.Data
+
+Namespace MasterDetailInside
+    Partial Public Class Window1
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
