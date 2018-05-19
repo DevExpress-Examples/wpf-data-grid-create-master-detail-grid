2f60d09e34f258ccfb81a298a30d276bb8a43a1e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/Application.xaml.vb
@@ -0,0 +1,11 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace MasterDetailInside
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+	End Class
+End Namespace
