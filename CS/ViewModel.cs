a7a1000f10d2baeb62a15f5d2ed7990c100354e5
Andrey Marten <andrey.kunitsyn@devexpress.com>
2020-12-28 21:06:14 +0300

    Use BindableBase and ObservableCollection

--- CS/ViewModel.cs
+++ CS/ViewModel.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 namespace MasterDetailInside {
     public class ViewModel {
-        List<ParentTestData> data;
-        public List<ParentTestData> Data {
+        ObservableCollection<ParentTestData> data;
+        public ObservableCollection<ParentTestData> Data {
             get {
                 if(data == null){
-                    data = new List<ParentTestData>();
+                    data = new ObservableCollection<ParentTestData>();
                     for(int i = 0; i < 100; i++) {
-                        ParentTestData parentTestData = new ParentTestData() { Text = "Master" + i, Number = i, Data = new List<TestData>() };
+                        ParentTestData parentTestData = new ParentTestData() { Text = "Master" + i, Number = i, Data = new ObservableCollection<TestData>() };
                         for(int j = 0; j < 50; j++)
                             parentTestData.Data.Add(new TestData() { Text = "Detail" + j + " Master" + i, Number = j, Ready = j % 2 != 0 });
                         data.Add(parentTestData);
@@ -22,14 +24,41 @@ namespace MasterDetailInside {
         }
     }
 
-    public class TestData {
-        public bool Ready { get; set; }
-        public string Text { get; set; }
-        public int Number { get; set; }
+    public class TestData : BindableBase
+    {
+        public bool Ready
+        {
+            get { return GetProperty(() => Ready); }
+            set { SetProperty(() => Ready, value); }
+        }
+        public string Text
+        {
+            get { return GetProperty(() => Text); }
+            set { SetProperty(() => Text, value); }
+        }
+        public int Number
+        {
+            get { return GetProperty(() => Number); }
+            set { SetProperty(() => Number, value); }
+        }
     }
-    public class ParentTestData {
-        public string Text { get; set; }
-        public int Number { get; set; }
-        public List<TestData> Data { get; set; }
+
+    public class ParentTestData : BindableBase
+    {
+        public string Text
+        {
+            get { return GetProperty(() => Text); }
+            set { SetProperty(() => Text, value); }
+        }
+        public int Number
+        {
+            get { return GetProperty(() => Number); }
+            set { SetProperty(() => Number, value); }
+        }
+        public ObservableCollection<TestData> Data
+        {
+            get { return GetProperty(() => Data); }
+            set { SetProperty(() => Data, value); }
+        }
     }
 }
