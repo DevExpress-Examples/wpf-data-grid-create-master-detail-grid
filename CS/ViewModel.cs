f0a007b1453094672804b41078e3a45a6b0d515a
CORP\ploskin.alexander <alexander.ploskin@devexpress.com>
2021-10-27 19:15:31 +0300

    Fix codestyle

--- CS/ViewModel.cs
+++ CS/ViewModel.cs
@@ -1,75 +1,39 @@
 ﻿using DevExpress.Mvvm;
-using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 
 namespace MasterDetailInside {
+    public class ParentDataItem : BindableBase {
+        public string Text { get { return GetValue<string>(); } set { SetValue(value); } }
+
+        public int Number { get { return GetValue<int>(); } set { SetValue(value); } }
+
+        public ObservableCollection<DataItem> Data { get { return GetValue<ObservableCollection<DataItem>>(); } set { SetValue(value); } }
+    }
+
+    public class DataItem : BindableBase {
+        public bool Ready { get { return GetValue<bool>(); } set { SetValue(value); } }
+
+        public string Text { get { return GetValue<string>(); } set { SetValue(value); } }
+
+        public int Number { get { return GetValue<int>(); } set { SetValue(value); } }
+    }
+
     public class ViewModel {
-        ObservableCollection<ParentTestData> _data;
-        public ObservableCollection<ParentTestData> Data {
-            get {
-                if(_data == null){
-                    _data = new ObservableCollection<ParentTestData>();
-                    for(int i = 0; i < 100; i++) {
-                        ParentTestData parentTestData = new ParentTestData() { Text = "Master" + i, Number = i, Data = new ObservableCollection<TestData>() };
-                        for(int j = 0; j < 50; j++)
-                            parentTestData.Data.Add(new TestData() { Text = "Detail" + j + " Master" + i, Number = j, Ready = j % 2 != 0 });
-                        _data.Add(parentTestData);
-                    }
+        public ObservableCollection<ParentDataItem> Data { get; set; }
+
+        public ViewModel() {
+            Data = new ObservableCollection<ParentDataItem>(GetData());
+        }
+
+        static IEnumerable<ParentDataItem> GetData() {
+            for(int i = 0; i < 100; i++) {
+                var parentTestData = new ParentDataItem() { Text = "Master" + i, Number = i, Data = new ObservableCollection<DataItem>() };
+                for(int j = 0; j < 50; j++) {
+                    parentTestData.Data.Add(new DataItem() { Text = "Detail" + j + " Master" + i, Number = j, Ready = j % 2 != 0 });
                 }
-                return _data;
+                yield return parentTestData;
             }
-        }
-    }
-
-    public class TestData : BindableBase
-    {
-
-        protected bool _Ready;
-        public bool Ready
-        {
-            get { return this._Ready; }
-            set { this.SetProperty(ref this._Ready, value, "Ready"); }
-        }
-
-        protected string _Text;
-        public string Text
-        {
-            get { return this._Text; }
-            set { this.SetProperty(ref this._Text, value, "Text"); }
-        }
-
-        protected int _Number;
-        public int Number
-        {
-            get { return this._Number; }
-            set { this.SetProperty(ref this._Number, value, "Number"); }
-        }
-    }
-
-    public class ParentTestData : BindableBase
-    {
-        protected string _Text;
-        public string Text
-        {
-            get { return this._Text; }
-            set { this.SetProperty(ref this._Text, value, "Text"); }
-        }
-
-        protected int _Number;
-        public int Number
-        {
-            get { return this._Number; }
-            set { this.SetProperty(ref this._Number, value, "Number"); }
-        }
-
-        protected ObservableCollection<TestData> _Data;
-        public ObservableCollection<TestData> Data
-        {
-            get { return this._Data; }
-            set { this.SetProperty(ref this._Data, value, "Data"); }
         }
     }
 }
